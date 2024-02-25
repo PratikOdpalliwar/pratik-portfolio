6e672fb4a270ff900daf390dabb3ff11eaa6f2ac
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-02-25 17:19:20 +0530

    project section is created

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.pexels.com",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['cdn.pixabay.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      // Add the new remote pattern for "cdn.pixabay.com" here
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
